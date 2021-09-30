e0b409174301a77cc88c4d088495e57e2dcf53bf
daviddeng78 <ddeng30@stuy.edu>
2021-09-30 13:15:25 -0400

    minor progress on new program

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,16 @@
+//DISCOVERIES
+
+
+public class Greet 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("No hablo queso");
+		greet("David");
+	}
+	
+	public static void greet(String)
+	{
+		System.out.println("Why, hello there, " + String + ". How do you do?")
+	}
+}
