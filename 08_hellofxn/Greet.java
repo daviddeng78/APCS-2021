a8aadcbe536fd9e1084b72428c63006a1a68f5c7
daviddeng78 <ddeng30@stuy.edu>
2021-09-30 18:27:00 -0400

    finished program

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,15 +1,21 @@
-//DISCOVERIES
-
-
 public class Greet 
 {
+	/**DISCOVERIES
+	 *
+	 */
+
+	/**UNRESOLVED QUESTIONS
+	 *
+	 */
 	public static void main(String[] args)
 	{
 		System.out.println("No hablo queso");
 		greet("David");
+		greet("Lea");
+		greet("Jacklyn");
 	}
 	
-	public static void greet(String)
+	public static void greet(String name)
 	{
 		System.out.println("Why, hello there, " + String + ". How do you do?")
 	}
