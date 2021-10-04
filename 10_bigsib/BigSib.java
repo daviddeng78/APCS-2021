e2ffc1d385655d497d8d1e4be907ded859b5f4b8
daviddeng78 <ddeng30@stuy.edu>
2021-10-04 17:37:10 -0400

    hw10

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,21 @@
+/**
+ *David Deng
+ *APCS
+ *HW10 -- Refactor Big Sib One / Utilizing Methods To Return A Certain Output
+ *2021-10-04
+ *
+ *
+ *DISCOVERIES
+ *If a certain Java file requires another Java file in order for it to execute, the required Java file will automatically compile.
+ *
+ *UNRESOLVED QUESTIONS
+ *No questions were unresolved.
+ */
+
+public class BigSib
+{
+	public static String greet(String name)
+	{
+		return("Why, hello there, " + name + ". How do you do?");
+	}
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,23 @@
+/**
+ *David Deng
+ *APCS
+ *HW10 -- Refactor Big Sib One / Utilizing Methods To Return A Certain Output
+ *2021-10-04
+ *
+ *
+ *DISCOVERIES
+ *If a certain Java file requires another Java file in order for it to execute, the required Java file will automatically compile.
+ *
+ *UNRESOLVED QUESTIONS
+ *No questions were unresolved.
+ */
+
+public class Greet
+{
+	public static void main(String[] args)
+	{
+		System.out.println(BigSib.greet("Mr. Mykolyk"));
+		System.out.println(BigSib.greet("Lea"));
+		System.out.println(BigSib.greet("Jacklyn"));
+	}
+}
