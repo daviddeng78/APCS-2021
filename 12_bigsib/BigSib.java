713ddc56ec111539a3525e308659317f44ece155
daviddeng78 <ddeng30@stuy.edu>
2021-10-05 19:45:54 -0400

    eliminated need for get method and modified heading

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,13 +1,13 @@
 /**
  *David Deng
  *APCS
- *HW12 -- On Elder Individuality and the Elimination of Radio Fuzz / Get and Set Methods, Instance Variables, this.
+ *HW12 -- On Elder Individuality and the Elimination of Radio Fuzz / Get Methods, Instance Variables, this.
  *2021-10-05
  *
  *
  *DISCOS
- *Get and set methods can be used to retrieve and modify instance variables.
- *this. refers to the object in the instance variable in the current method.
+ *Set methods can be used to modify instance variables.
+ *this. refers to the instance variable in the current method.
  *
  *QCCs
  *No questions were unresolved.
@@ -23,13 +23,8 @@ public class BigSib
 		this.msg = msg;
 	}
 
-	public String getMsg()
-	{
-		return msg;
-	}
-
 	public String greet(String noun)
 	{
-		return getMsg() + " " + noun;
+		return msg + " " + noun;
 	}
 }
--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,17 +1,18 @@
 /**
  *David Deng
  *APCS
- *HW12 -- On Elder Individuality and the Elimination of Radio Fuzz / Get and Set Methods, Instance Variables, this.
+ *HW12 -- On Elder Individuality and the Elimination of Radio Fuzz / Set Methods, Instance Variables, this.
  *2021-10-05
  *
  *
  *DISCOS
- *Get and set methods can be used to retrieve and modify instance variables.
- *this. refers to the object in the instance variable in the current method.
+ *Set methods can be used to modify instance variables.
+ *this. refers to the instance variable in the current method.
  *
  *QCCs
- *No questions were unresolved.
+ *No unresolved questions
  */
+
 
 public class Greet
 {
