4c0d0db41566dbafd4bcdb02ce37dee51f13b7d7
daviddeng78 <ddeng30@stuy.edu>
2021-10-25 21:36:17 -0400

    reverted back to three separate methods with changes to first method

--- 24_while/Driver.java
+++ 24_while/Driver.java
@@ -21,28 +21,63 @@
 
 public class Driver {
 
-	public static void loop(Coin c1, Coin c2, int x, int y, int birthyear)
+	public static void headCount(Coin c1, Coin c2, int max)
 	{
-		int matchCtr = 0;
-		boolean start = true;
 		int headsCt = c1.getHeadsCtr() + c2.getHeadsCtr();
 		int flips = 0;
-		while (start)
+		while (headsCt < max)
 		{
-			if ((matchCtr > 65536) && (matchCtr % birthyear == 0) && (headsCt >= x) && (matchCtr >= y))
-			{
-				start = false;
-			}
+			c1.flip();
+			c2.flip();
+			headsCt = c1.getHeadsCtr() + c2.getHeadsCtr();
+			flips += 1;
+		}
+		System.out.println("It took " + flips + " flips for " + max + " heads to come up.");
+	}
+
+	public static void matchCount(Coin c1, Coin c2, int matches)
+	{
+		int matchCtr = 0;
+		int flips = 0;
+		while (matchCtr < matches)
+		{
 			c1.flip();
 			c2.flip();
 			if (c1.equals(c2))
 			{
 				matchCtr += 1;
 			}
-			headsCt = c1.getHeadsCtr() + c2.getHeadsCtr();
 			flips += 1;
 		}
-		System.out.println("It took " + flips + " flips for all conditions to be fulfilled.");
+		System.out.println("It took " + flips + " flips for " + matchCtr + " matches to occur.");
+	}
+
+	public static void yearMatch(Coin c1, Coin c2, int birthyear)
+	{
+		int matchCtr = 0;
+		int flips = 0;
+		while (matchCtr < 65536)
+		{
+			c1.flip();
+			c2.flip();
+			if (c1.equals(c2))
+			{
+				matchCtr += 1;
+			}
+			flips += 1;
+		}
+
+		while (matchCtr % birthyear != 0)
+		{
+			c1.flip();
+			c2.flip();
+			if (c1.equals(c2))
+			{
+				matchCtr += 1;
+			}
+			flips += 1;
+		}
+		System.out.println("It took " + flips + " flips for the # of matches to be greater than 65536 and completely divisible by " + birthyear + ".");
 	}
 
 	public static void main( String[] args ) {
@@ -53,25 +88,20 @@ public class Driver {
 		//test default constructor
 		/*===================TOP==========================
 		Coin mine = new Coin();
-
       		//test 1st overloaded constructor
       		Coin yours = new Coin( "quarter" );
-
       		//test 2nd overloaded constructor
       		Coin wayne = new Coin( "dollar", "heads" );
-
       		//test toString() methods of each Coin
       		System.out.println("mine: " + mine);
       		System.out.println("yours: " + yours);
       		System.out.println("wayne: " + wayne);
-
       		//test flip() method
       		System.out.println("\nAfter flipping...");
       		yours.flip();
       		wayne.flip();
       		System.out.println("yours: " + yours);
       		System.out.println("wayne: " + wayne);
-
       		//test equals() method
       		if ( yours.equals(wayne) ) {
        			System.out.println( "Matchee matchee!" );
@@ -82,7 +112,10 @@ public class Driver {
       		====================BOTTOM======================*/
 		Coin coin1 = new Coin("penny", "heads");
 		Coin coin2 = new Coin("quarter", "heads");
-		loop(coin1, coin2, 70000, 69000, 2005);
+		headCount(coin1, coin2, 10);
+		matchCount(coin1, coin2, 10);
+		yearMatch(coin1, coin2, 2005);
   	}//end main()
 
-}//end class
+}//end classpublic class Driver {
+
